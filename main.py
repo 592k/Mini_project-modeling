2c954227869ff5c1b1e3a75d90016a66e28020ec
592k <kweonok94@gmail.com>
2022-06-16 16:58:19 +0900

    test main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #%%
 from dataloader import *
 from preprocesses.preprocess_V import *
@@ -5,3 +6,15 @@ from preprocesses.preprocess_V import *
 X, y, test_merge = load_data()
 X, test_merge = pre_V(X, test_merge)
 # %%
+=======
+# from dataloaer import 
+# from preprocess import 
+# from utils import load_config
+
+def main():
+
+    print('Done')
+
+if __name__ == "__main__":
+    main()
+>>>>>>> 272e0e0 (test main.py)
