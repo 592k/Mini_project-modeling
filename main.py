af38731cf87767e5a1fb84c11dadb44dec1689d9
Farmer-from-Space <nofx2002@gmail.com>
2022-06-16 18:26:24 +0900

    building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #%%
 from dataloader import *
 from preprocesses.preprocess_V import *
@@ -7,6 +8,8 @@ X, y, test_merge = load_data()
 X, test_merge = pre_V(X, test_merge)
 # %%
 =======
+=======
+>>>>>>> 4869af3 (building)
 # from dataloaer import 
 # from preprocess import 
 # from utils import load_config
@@ -17,4 +20,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+<<<<<<< HEAD
 >>>>>>> 272e0e0 (test main.py)
+=======
+=======
+#%%
+from dataloader import *
+from preprocesses.preprocess_V import *
+
+X, y, test_merge = load_data()
+X, test_merge = pre_V(X, test_merge)
+# %%
+>>>>>>> 6304e40 (building)
+>>>>>>> 4869af3 (building)
