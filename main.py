903133f45bf1ff88072c62ef79f6c4809df23524
Farmer-from-Space <nofx2002@gmail.com>
2022-06-17 10:38:44 +0900

    del again

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#%%
-from dataloader import *
-from preprocesses.preprocess_V import *
-
-X, y, test_merge = load_data()
-X, test_merge = pre_V(X, test_merge)
-# %%
-=======
-=======
->>>>>>> 4869af3 (building)
 # from dataloaer import 
 # from preprocess import 
 # from utils import load_config
 
-def main():
+def mpreprocess.preprocess_V
 
     print('Done')
 
 if __name__ == "__main__":
     main()
-<<<<<<< HEAD
->>>>>>> 272e0e0 (test main.py)
-=======
-=======
-#%%
-from dataloader import *
-from preprocesses.preprocess_V import *
-
-X, y, test_merge = load_data()
-X, test_merge = pre_V(X, test_merge)
-# %%
->>>>>>> 6304e40 (building)
->>>>>>> 4869af3 (building)
+preprocess.preprocess_V
