246f0d43c9cb8e7d3e05301880214aca95a65e79
592k <kweonok94@gmail.com>
2022-06-17 17:53:56 +0900

    fix: fixed code

--- preprocesses/preprocess_C.py
+++ preprocesses/preprocess_C.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 def pre_C(X, test_merge):
 
-    X.drop()
-    for i in ['C1','C9']:
-        
-        fenc = X[i].quantile(.75) + (3 * (X[i].quantile(.75) - X[i].quantile(.25)))
-        X.drop(X[i][X[i]>fenc].index, inplace=True)
+    # C1, C9 이상치 제거    
+    fenc1 = X['C1'].quantile(.75) + (3 * (X['C1'].quantile(.75) - X['C1'].quantile(.25)))
+    fenc2 = X['C9'].quantile(.75) + (3 * (X['C9'].quantile(.75) - X['C9'].quantile(.25)))
+    X.drop(X['C1'][X['C1']>fenc1].index, inplace=True)
+    X.drop(X['C9'][X['C9']>fenc2].index, inplace=True)
+
+    return X, test_merge
